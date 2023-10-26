dc6ef6717c12fc058841432c9f53e1379a0dfa6b
Peter3255 <YOUR_EMAIL>
2023-10-25 22:18:41 -0700

    commited

---
+++ Resources/Images/main.js
@@ -0,0 +1,22 @@
+let nextOne = document.getElementById('surveyButton')
+
+
+
+
+nextOne.onclick = function() {
+    nextTwo.hidden = false;
+    nextOne.hidden = true;
+
+}
+
+
+
+
+
+
+
+
+let nextTwo = document.getElementById('emailAddress')
+
+
+nextTwo.hidden = true;
